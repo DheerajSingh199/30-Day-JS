2caedfec535824f1bc3f8dceafdaeec321422fe1
DheerajSingh199 <dheerajsingh16496@gmail.com>
2024-07-14 01:17:41 +0530

    first commit

---
+++ scrpit.js
@@ -0,0 +1,36 @@
+var number = 2
+console.log(number);
+
+let text = "hello"
+console.log(text);
+
+const s  = true
+console.log(s);
+
+
+let a = 1
+console.log(typeof a);
+
+let b = "abcd"
+console.log(typeof b);
+
+let c = true
+console.log(typeof c);
+
+
+let d = {
+    name:"abc",
+    class:12
+}
+console.log(typeof d);
+
+
+let e = [1,23,45]
+console.log(typeof e); //object
+
+
+const ac = 2
+
+ac = 5
+console.log(ac);// errr
+
