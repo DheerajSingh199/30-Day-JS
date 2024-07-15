bb602ae69d4876811672aadf55b96c7ce6d6b242
DheerajSingh199 <dheerajsingh16496@gmail.com>
2024-07-15 22:50:34 +0530

    Day 2

---
+++ day2.js
@@ -0,0 +1,129 @@
+// let b = (a,b) => {
+// return a+b
+// }
+
+// let d = b(2,3)
+// let e = b(8,3)
+// console.log(d );
+
+// console.log(e);
+
+
+// let sub = (a,b) => {
+//     return a-b
+//     }
+
+// let res = sub(19,14)
+// console.log(res);
+
+
+// let mul = (a,b) => {
+//     return a*b
+//     }
+// let resd = mul(10,2)
+//     console.log(resd);
+
+
+// let dive = (a,b) => {
+// return a/b
+// }
+
+// let divd = dive(10,5)
+// console.log(divd);
+
+
+// let dives = (a,b) => {
+//     return a%b
+//     }
+    
+//     let divds = dive(900,100)
+//     console.log(divds);
+
+
+// let plus1 = 3
+// console.log(plus1+= 1);
+// let minus2 = 3
+// console.log(minus2-= 1);
+
+
+// let comp = (a,b) => {
+// return a>b
+// }
+// let dsc = comp(2,1)
+// console.log(dsc);
+
+
+// let comps = (a,b) => {
+//     return a<b
+//     }
+    
+// let dscs = comps(2,1)
+//  console.log(dscs);
+
+
+//  let comsp = (a,b) => {
+//     return a>=b
+//     }
+//     let dssc = comp(2,2)
+//     console.log(dssc);
+    
+    
+//     let compss = (a,b) => {
+//         return a<=b
+//         }
+        
+//     let dscss = comps(2,12)
+//      console.log(dscss);
+
+
+// let compereN = (a,b) => {
+//     console.log(a);
+//     console.log(b);
+
+
+//     return a==b
+
+// }
+// let dsxs = compereN("43",43)
+// console.log(dsxs);
+
+
+
+// let compereNs = (a,b) => {
+//     return a === b
+// }
+// let dsxsss = compereNs("43",43)
+// console.log(dsxsss);
+
+
+// let bszx =(a,b) =>{
+
+//     return a>2 && b>2
+// } 
+
+// let sadc = bszx(5,3)
+// console.log(sadc);
+
+
+// let bszx =(a,b) =>{
+
+//     return a>2 || b>2
+// } 
+
+// let sadc = bszx(5,3)
+// console.log(sadc);
+
+let bszx =(a) =>{
+
+    return !a
+} 
+
+let sadc = bszx(true)
+console.log(sadc);
+
+let cxz = (isnc) => {
+    return isnc ? "sda" : "minushoon"
+}
+
+let logzx = cxz(1)
+console.log(logzx);
