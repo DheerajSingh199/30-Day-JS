c72b2c092836168d92b3124993f125fe594f8f95
DheerajSingh199 <dheerajsingh16496@gmail.com>
2024-07-23 23:42:26 +0530

    Day3

---
+++ day3.js
@@ -0,0 +1,127 @@
+// // let v = (num) => {
+// //   if (num < 0) {
+// //     return console.log("Negative Number");
+// //   }
+// //   if (num > 0) {
+// //     return console.log("Positive Number");
+// //   }
+// // };
+
+// // let c = v(1)
+
+// // console.log(c);
+
+// // let sas = (age) => {
+// //     if(age >= 18){
+// //         return console.log("Eligible");
+// //     }else{
+// //         return console.log("Not Eligible");
+// //     }
+// // }
+
+// // let szx = sas(9)
+// // console.log(szx);
+
+// // let first = 1;
+// // let two = 21;
+// // let three = 4;
+
+// // if (first > two && first > three) {
+// //   console.log(first);
+// // } else if (two > first && two > three) {
+// //   console.log(two);
+// // } else {
+// //   console.log(three);
+// // }
+
+// // let v = 5;
+// // switch (v) {
+// //   case 0:
+// //     console.log(`i am ${v}`);
+// //     break;
+// //   case 1:
+// //     console.log(`i am ${v}`);
+// //     break;
+
+// //   case 2:
+// //     console.log(`i am ${v}`);
+// //     break;
+
+// //   case 3:
+// //     console.log(`i am ${v}`);
+// //     break;
+
+// //   case 4:
+// //     console.log(`i am ${v}`);
+// //     break;
+// //   case 5:
+// //     console.log(`i am ${v} & abc`);
+// //     break;
+// //   case 6:
+// //     console.log(`i am ${v}`);
+// //     break;
+
+// //   default:
+// //     break;
+// // }
+
+
+
+
+
+
+
+
+
+
+// // let grade = 34;
+// // let pass;
+
+// // switch (true) {
+// //   case grade > 0 && grade < 49:
+// //     pass = "D";
+
+// //     break;
+
+// //   case grade > 50 && grade < 69:
+// //     pass = "C";
+
+// //     break;
+
+// //   case grade > 70 && grade < 89:
+// //     pass = "B";
+
+// //     break;
+
+// //   case grade > 90 && grade < 100:
+// //     pass = "A";
+
+// //     break;
+
+// //   default:
+// //     break;
+// // }
+
+// // console.log(pass);
+
+
+
+// let b  = 3;
+// let fs = (b % 2 === 0 ) ? "even" : "odd"
+
+// console.log(fs);
+
+
+
+// let leapYear = 2016;
+// if (leapYear % 4 == 0 && leapYear % 100 !== 0 && leapYear % 400 !== 0) {
+//   console.log(`${leapYear}  is  leap year`);
+//   console.log(leapYear % 4);
+//   console.log(leapYear % 100);
+//   console.log(leapYear % 400);
+// } else {
+//   console.log(`${leapYear}  is  not leap year`);
+//   console.log(leapYear % 4);
+//   console.log(leapYear % 100);
+//   console.log(leapYear % 400);
+// }
